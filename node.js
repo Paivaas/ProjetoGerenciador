43fb4edefd1092a97bb064c9eb711e0aaec7e68d
Julia Paiva <juliapoliver12@gmail.com>
2024-03-07 10:13:45 -0300

    07.03

--- node.js
+++ node.js
@@ -1,4 +1,5 @@
 const button = document.getElementById('login')
+const header = document.getElementById('header')
 
 async function validarLogin(){
 
@@ -7,8 +8,28 @@ async function validarLogin(){
 
     if(email == '' || senha == ''){
 
-        alert('Valores invalidos')
-       
+        // alert('Valores invalidos')
+        const toast = document.createElement('section')
+        const icon = document.createElement('i')
+        const mensagem = document.createElement('p')
+
+        toast.className = 'toast'
+        icon.className = 'bi bi-exclamation-circle-fill'
+        mensagem.textContent = 'Os dados inseridos estao incorretos'
+
+        // icon.appendChild(toast)
+        toast.appendChild(icon)
+        toast.appendChild(mensagem)
+
+        header.appendChild(toast)
+
+        toast.addEventListener('click', sumir)
+
+        function sumir(){
+            toast.classList.add('toast-sumir')
+        }
+
+
     }else{
         const users = await fetch('http://localhost:8080/usuario')
         const listUsers = await users.json()
